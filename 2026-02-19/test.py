9d6ecbc4b84fec43c242d3e3f91537c5b688b104
gunil123 <202130115@email.daelim.ac.kr>
2026-02-19 15:57:43 +0900

    첫 번째 커밋

---
+++ 2026-02-19/test.py
@@ -0,0 +1,37 @@
+# #파이썬 자료형
+# ```
+# int 정수
+# float 실수
+# complex 복소수
+# bool 불린
+# str 문자열
+# list 리스트
+# tuple 튜플
+# set 집합
+# dict 사전(딕셔너리)
+# ```
+
+# 데이터 타입
+str1="Python"
+bool1=True
+float1=10.4
+int1=3
+list1=[str1]
+dict1={"name":"gindong","id":3}
+tuple1=4,2,4
+set1={1,23,4}
+
+# 데이터 타입 출력
+print(type(str1))  # <class 'str'>
+print(type(bool1)) # <class 'bool'>
+print(type(float1)) # <class 'float'>
+print(type(int1)) #<class 'int'>
+print(type(list1)) #<class 'list'>
+print(type(dict1)) # <class 'dict'>
+print(type(tuple1)) # <class 'tuple'>
+print(type(set1)) # <class 'set'>
+
+#str1 = "Python"
+#str1이라는 문자자체를 객체로 만들고 다양한 속성(변수), 행동(함수)를 넣을 수 있다.
+
+
